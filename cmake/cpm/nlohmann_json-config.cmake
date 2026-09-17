9a2bd43057d91d5e7111b2f48bcf0452eaa04dd4
evgeniy.gleba <evgeniy.gleba@ya.ru>
2026-09-17 22:55:45 +0300

    add basic schema parser

---
+++ cmake/cpm/nlohmann_json-config.cmake
@@ -0,0 +1,10 @@
+cpmaddpackage(
+    NAME
+    nlohmann_json
+    GITHUB_REPOSITORY
+    nlohmann/json
+    GIT_TAG
+    v3.12.0
+    OPTIONS
+    "JSON_BuildTests OFF"
+    "JSON_MultipleHeaders OFF")
